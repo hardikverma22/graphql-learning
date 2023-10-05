acaad95d37620f040e53fced63f8b5b580b922ca
Hardik Verma <hardikverma22@rediffmail.com>
2023-10-05 20:39:10 +0530

    first commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
+  const conn = await mongoose.connect('mongodb+srv://hardikverma22:Hdk1234%40@cluster0.5txsqko.mongodb.net/?retryWrites=true&w=majority');
 
   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
 };
 
 module.exports = connectDB;
+
+
